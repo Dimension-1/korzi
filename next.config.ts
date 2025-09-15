5eb2523f80435395c13809660d7d452d4d2c50fa
Arnab <arnabpramanik102018@gmail.com>
2025-09-15 12:04:16 +0530

    all

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     unoptimized: true,
+    domains: ['ap-south-1.cdn.hygraph.com'],
     remotePatterns: [
       {
         protocol: 'https',
